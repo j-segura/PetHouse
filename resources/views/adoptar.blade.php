ae2cb163833d277e2b0df031a51e924ff56b5c9a
j-segura <juanda.segura2005@gmail.com>
2022-09-21 13:20:18 -0500

    segunda parte

--- resources/views/adoptar.blade.php
+++ resources/views/adoptar.blade.php
@@ -1,23 +1,129 @@
 <x-app-layout>
-    <div class="header_adopcion">
-        <div class="header_data">
-            <h1>¿Quieres adoptar?</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book
-            </p>
-            <a href="" class="ver_candidatos">
-                Ver candidatos
-                <i class="fa-solid fa-angle-down"></i>
-            </a>
+
+    <section class="banner">
+        <video src="{{asset('video/video.mp4')}}" muted autoplay loop></video>
+        <div class="data">
+            <h1>PETHOUSE</h1>
+            <p>Estas buscando un compañero</p>
+            <a href="#header">Ver Candidatos</a>
         </div>
-        <div class="home_image">
-            <svg class="home_blob" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#8A3FFC"
-                    d="M53.3,-51.2C67.3,-39.2,75.9,-19.6,73.7,-2.2C71.5,15.3,58.6,30.5,44.6,46.6C30.5,62.7,15.3,79.6,-0.5,80.1C-16.3,80.6,-32.7,64.8,-47.6,48.7C-62.6,32.7,-76.1,16.3,-73.5,2.7C-70.8,-11,-51.9,-22,-36.9,-33.9C-22,-45.9,-11,-58.8,4.3,-63.1C19.6,-67.4,39.2,-63.1,53.3,-51.2Z"
-                    transform="translate(100 100)" />
-                <img src="{{ asset('img/gog4.png') }}" class="home_blob_img">
-            </svg>
-        </div>
+    </section>
+
+    <div id="header">
+        <h1>¡Tu Futuro Compañero Esta Aqui!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis ullam repellat sequi, placeat nesciunt quas eum cum nam.</p>
     </div>
+
+    <div id="filter">
+        <ul>
+            <li><a href="">Todos</a></li>
+            <li><a href="">Perros</a></li>
+            <li><a href="">Gatos</a></li>
+        </ul>
+    </div>
+
+    <section id="adoptar">
+        <div class="animals">
+
+            <!-- card_1 -->
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal6.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Stella</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal10.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Flor</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <!-- card_2 -->
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal7.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Duke</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <!-- card_2 -->
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal3.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Winter</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <!-- card_2 -->
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal9.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Pixie</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal11.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">April</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <!-- card_2 -->
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal5.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Dash</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal7.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Dash</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img">
+                    <img src="{{ asset('img/animals/animal8.jpg') }}" alt="">
+                </div>
+                <div class="card_contenido">
+                    <h2 class="name">Emma</h2>
+                    <p class="tags">Pit Bull, Hembra, 3 años</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </x-app-layout>
+
+<style>
+
+</style>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,7 +1,7 @@
 @php
 $nav_links = [
     [
-        'name' => 'Adoptar',
+        'name' => 'Inicio',
         'route' => route('adoptar'),
         'active' => request()->routeIs('adoptar'),
     ],
